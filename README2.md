604c0fda6812fad8c31ce393cda4586a2995f6ff
Gerardo Solis <gerardoa.solis@yahoo.com>
2020-09-30 22:49:45 -0700

    prompt and module function to denerate markdown created

---
+++ README2.md
@@ -0,0 +1,22 @@
+# example title
+  ## Description
+  example description 
+  ## Table of Contents
+  * item1
+  * item2
+  * item3
+  ## Installation
+  undefined
+  ## Usage
+  undefined
+  ## License
+  undefined
+  ## Contributing
+  undefined
+  ## Tests
+  undefined
+  ## Questions
+  <a href=undefined>undefined</a>
+  
+
+
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,37 @@
+
+
 // function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+function generateMarkdown(questions) {
+  return `# ${questions.title}
+  ## Description
+  ${questions.description}
+  ## Table of Contents
+  ${splitQuestions = questions.tableOfContents.split(" ").forEach(
+
+    function(){return "*" + splitQuestions }
+    
+  )}
+  ## Installation
+  ${questions.installation}
+  ## Usage
+  ${questions.usage}
+  ## License
+  ${questions.license}
+  ## Contributing
+  ${questions.contributing}
+  ## Tests
+  ${questions.tests}
+  ## Questions
+  <a href=${questions.username}>${questions.username}</a>
+  
+
 
 `;
 }
 
 module.exports = generateMarkdown;
+
+// ## Table of Contents
+  // * ${questions.tableOfContents.split(" ")[0]}
+  // * ${questions.tableOfContents.split(" ")[1]}
+  // * ${questions.tableOfContents.split(" ")[2]}
